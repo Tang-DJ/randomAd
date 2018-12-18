e97e5d61fb55a662eebe0825577fe1bb1fa5847a
kristangdaijia_i <kristangdaijia_i@didichuxing.com>
2018-12-18 17:36:03 +0800

    新增server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-header>randomAD</el-header>
+    <el-container>
+      <el-upload
+              class="upload-demo"
+              drag
+              action= "https://jsonplaceholder.typicode.com/posts/"
+              multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传txt文件</div>
+      </el-upload>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
+  },
+  data(){
+
+  },
+  methods: {
+      uploadTxt: function (val) {
+         console.log(val);
+      }
   }
 }
 </script>
